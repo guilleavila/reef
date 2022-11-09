32af22efac3f36a1acaf98913516ff0215641ef6
Guille Ávila <guille9797@hotmail.com>
2022-11-09 12:59:40 +0100

    added depth parallax

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles.css'
+
+const createDepth = (e) => {
+    document.querySelectorAll('.plane').forEach(plane => {
+        const speed = plane.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX * speed) / 100
+        const y = (window.innerHeight - e.pageY * speed) / 100
+
+        // conseguir el 50% de las medidas
+
+
+
+        const { width, height } = plane.getBoundingClientRect()
+        console.log(width, height)
+
+        plane.style.transform = `translateX(${(width / 2) + x}px) translateY(-${(height / 2) + y}px)`
+    });
+}
+
+document.addEventListener('mousemove', createDepth)
+
