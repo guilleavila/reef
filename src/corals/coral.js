4a89a3f46efa3726ae03766addf4f2e3d9a67179
Guille Ávila <guille9797@hotmail.com>
2022-11-09 21:56:02 +0100

    refactored classes and added first sprite-coral

---
+++ src/corals/coral.js
@@ -0,0 +1,141 @@
+import gsap from "gsap"
+import { CustomEase } from "gsap/all"
+
+gsap.registerPlugin(CustomEase)
+
+class Coral {
+    constructor(posX, posY, width, speed, id) {
+        this.position = { x: posX, y: posY }
+        this.width = width
+        this.speed = speed
+        this.id = id
+        this.isAnimated = false
+    }
+}
+
+export class SpikedCoral extends Coral {
+    constructor(posX, posY, width, speed, id) {
+        super(posX, posY, width, speed, id)
+    }
+
+    draw() {
+        const coralImage = document.createElement('img')
+        const attributes = {
+            'id': this.id,
+            'src': './images/scene1/sprites/spiked-coral/spiked-coral.png',
+            'alt': 'spiked-coral',
+            'style': `position: fixed; top: ${this.position.y}%; left: ${this.position.x}%; width: ${this.width}%;`,
+            'class': 'plane',
+            'data-speed': this.speed
+        }
+
+        for (const attr in attributes) {
+            coralImage.setAttribute(attr, attributes[attr])
+        }
+
+        document.body.appendChild(coralImage)
+        coralImage.addEventListener('mouseover', this.animate)
+    }
+
+    animate() {
+        if (!this.isAnimated) {
+            this.isAnimated = true
+            gsap.to(`#${this.id}`, {
+                scale: 0,
+                rotate: 360,
+                duration: 2,
+                ease: CustomEase.create("custom", "M0,0 C0.126,0.382 0.106,0.626 0.208,0.638 0.658,0.691 0.818,0.001 1,0 "),
+                onComplete: () => this.isAnimated = false
+            })
+        }
+    }
+}
+
+export class ShinyCoral extends Coral {
+    constructor(posX, posY, width, speed, id, color) {
+        super(posX, posY, width, speed, id)
+        this.color = color
+    }
+
+    draw() {
+        const coralImage = document.createElement('img')
+        const attributes = {
+            'id': this.id,
+            'src': `./images/scene1/sprites/shiny-coral/${this.color}.png`,
+            'alt': `${this.color}-shiny-coral`,
+            'style': `position: fixed; top: ${this.position.y}%; left: ${this.position.x}%; width: ${this.width}%;`,
+            'class': 'plane',
+            'data-speed': this.speed
+        }
+
+        for (const attr in attributes) {
+            coralImage.setAttribute(attr, attributes[attr])
+        }
+
+        document.body.appendChild(coralImage)
+        coralImage.addEventListener('mouseover', this.animate)
+    }
+
+    animate() {
+        if (!this.isAnimated) {
+            console.log(this.scaleF)
+            this.isAnimated = true
+            gsap.to(`#${this.id}`, {
+                scale: 0.6,
+                duration: 1,
+                ease: CustomEase.create("custom", "M0,0 C0.126,0.382 0.085,0.988 0.302,0.988 0.722,0.988 0.818,0.001 1,0 "),
+                onComplete: () => this.isAnimated = false
+            })
+        }
+    }
+}
+
+export class ShinyCoralP2 extends ShinyCoral {
+    constructor(posX, posY, width, color, speed, id) {
+        super(posX, posY, width, color, speed, id)
+    }
+
+    animate() {
+        if (!this.isAnimated) {
+            console.log(this.scaleF)
+            this.isAnimated = true
+            gsap.to(`#${this.id}`, {
+                scale: 0.8,
+                duration: 1,
+                ease: CustomEase.create("custom", "M0,0 C0.126,0.382 0.085,0.988 0.302,0.988 0.722,0.988 0.818,0.001 1,0 "),
+                onComplete: () => this.isAnimated = false
+            })
+        }
+    }
+}
+
+export class SpriteCoral extends Coral {
+
+    constructor(posX, posY, width, speed, id, coralFrames) {
+        super(posX, posY, width, speed, id)
+        this.coralFrames = coralFrames
+        this.framesIndex = 1
+    }
+
+    draw() {
+        const coralImage = document.createElement('img')
+        const attributes = {
+            'id': this.id,
+            'src': `./images/scene1/sprites/orange-heart-coral/sprite-coral-corazon-naranja-${this.framesIndex}.png`,
+            'alt': 'sprite-coral',
+            'style': `position: fixed; top: ${this.position.y}%; left: ${this.position.x}%; width: ${this.width}%;`,
+            'class': 'plane',
+            'data-speed': this.speed
+        }
+
+        for (const attr in attributes) {
+            coralImage.setAttribute(attr, attributes[attr])
+        }
+
+        document.body.appendChild(coralImage)
+    }
+
+    animate(framesCounter) {
+
+    }
+}
--- src/scene1.js
+++ src/scene1.js
@@ -1,9 +1,11 @@
-import ShinyCoral, { ShinyCoralP2 } from "./corals/shiny-coral"
-import SpikedCoral from "./corals/spiked-coral"
+import { ShinyCoral, ShinyCoralP2, SpikedCoral, SpriteCoral } from "./corals/coral"
 
 const scene1 = {
+    p1Speed: 0.5,
+    p2Speed: 0.3,
     shinyCorals: [],
     spikedCorals: [],
+    spriteCorals: [],
 
     init() {
         this.createShinyCorals()
@@ -11,6 +13,9 @@ const scene1 = {
 
         this.createSpikedCorals()
         this.spikedCorals.forEach(spikedCoral => spikedCoral.draw())
+
+        this.createSpriteCorals()
+        this.spriteCorals.forEach(spriteCoral => spriteCoral.draw())
 
         document.addEventListener('mousemove', this.createDepth)
     },
@@ -28,17 +33,22 @@ const scene1 = {
     },
 
     createShinyCorals() {
-        this.shinyCorals.push(new ShinyCoral(42, 1, 120, 'pink', 0.5, 'shiny-1'))
-        this.shinyCorals.push(new ShinyCoral(30, 77, 105, 'green', 0.5, 'shiny-2'))
-        this.shinyCorals.push(new ShinyCoral(51, 7, 105, 'blue', 0.5, 'shiny-3'))
+        this.shinyCorals.push(new ShinyCoralP2(47, -1, 5, this.p2Speed, 'shiny-4', 'blue-2'))
+        this.shinyCorals.push(new ShinyCoralP2(48, 8, 4, this.p2Speed, 'shiny-5', 'blue-2'))
+        this.shinyCorals.push(new ShinyCoralP2(-7, 87, 6, this.p2Speed, 'shiny-6', 'blue-2'))
 
-        this.shinyCorals.push(new ShinyCoralP2(47, -1, 130, 'blue-2', 0.3, 'shiny-4'))
-        this.shinyCorals.push(new ShinyCoralP2(48, 8, 80, 'blue-2', 0.3, 'shiny-5'))
-        this.shinyCorals.push(new ShinyCoralP2(-7, 87, 150, 'blue-2', 0.3, 'shiny-6'))
+        this.shinyCorals.push(new ShinyCoral(42, 1, 5, this.p1Speed, 'shiny-1', 'pink'))
+        this.shinyCorals.push(new ShinyCoral(30, 77, 5, this.p1Speed, 'shiny-2', 'green'))
+        this.shinyCorals.push(new ShinyCoral(51, 7, 5, this.p1Speed, 'shiny-3', 'blue'))
+
     },
 
     createSpikedCorals() {
-        this.spikedCorals.push(new SpikedCoral(85, 59, 250, 0, 1, 'spiked-1'))
+        this.spikedCorals.push(new SpikedCoral(85, 59, 10, this.p1Speed, 'spiked-1'))
+    },
+
+    createSpriteCorals() {
+        this.spriteCorals.push(new SpriteCoral(7, 54, 15, this.p1Speed, 'sprite-coral-1', 15))
     }
 }
 
