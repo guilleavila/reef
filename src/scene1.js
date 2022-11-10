08faa505fa18e5c0219f48f55c8b5c73a57e6bea
Guille Ávila <guille9797@hotmail.com>
2022-11-10 13:06:29 +0100

    added stingray and created movement paths

--- src/scene1.js
+++ src/scene1.js
@@ -1,11 +1,16 @@
+import gsap from "gsap"
+import { CustomEase, MotionPathPlugin } from "gsap/all"
 import { ShinyCoral, ShinyCoralP2, SpikedCoral, SpriteCoral } from "./corals/coral"
+import { Stingray } from "./stingray/stingray"
+
+gsap.registerPlugin(MotionPathPlugin)
 
 const scene1 = {
     p1Speed: 0.5,
     p2Speed: 0.3,
     framesCounter: 0,
     intervalId: undefined,
-    stingRay: undefined,
+    stingrays: [],
     shinyCorals: [],
     spikedCorals: [],
     spriteCorals: [],
@@ -20,7 +25,11 @@ const scene1 = {
         this.createSpriteCorals()
         this.spriteCorals.forEach(spriteCoral => spriteCoral.draw())
 
+        this.createStingrays()
+        this.stingrays.forEach(stingray => stingray.draw())
+
         this.sceneLoop()
+        this.stingRaySwim()
 
         document.addEventListener('mousemove', this.createDepth)
     },
@@ -29,6 +38,7 @@ const scene1 = {
         this.intervalId = setInterval(() => {
             this.framesCounter >= 600 ? this.framesCounter = 0 : this.framesCounter++
             this.spriteCorals.forEach(coral => coral.animate(this.framesCounter, coral.id))
+            this.stingrays.forEach(stingray => stingray.animate(this.framesCounter, stingray.id))
         }, 1000 / 60)
     },
 
@@ -72,6 +82,36 @@ const scene1 = {
         // -- WHITE CORAL --
         this.spriteCorals.push(new SpriteCoral(10, 54, 12, this.p1Speed, 'white-coral-1', 15, 'p1', 'white-coral'))
         this.spriteCorals.push(new SpriteCoral(75, 80, 15, this.p2Speed, 'white-coral-2', 15, 'p2', 'white-coral'))
+    },
+
+    createStingrays() {
+        this.stingrays.push(new Stingray(0, 0, 25, 'stingray-1', 50, 'p1', 'stingray'))
+    },
+
+    stingRaySwim() {
+
+        gsap.timeline()
+            .to('#stingray-1', {
+                duration: 15,
+                motionPath: {
+                    path: "#path-1",
+                    align: "#path-1",
+                    alignOrigin: [0.5, 0.5],
+                    autoRotate: 90
+                },
+                ease: "linear"
+            })
+            .to('#stingray-1', {
+                duration: 15,
+                motionPath: {
+                    path: "#path-2",
+                    align: "#path-2",
+                    alignOrigin: [0.5, 0.5],
+                    autoRotate: 90
+                },
+                ease: "linear",
+                delay: 2
+            })
     }
 }
 
