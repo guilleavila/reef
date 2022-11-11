630caad82b300bc69475e2ef511e0b6f73e9174d
Guille Ávila <guille9797@hotmail.com>
2022-11-11 22:35:45 +0100

    added fish and intro screen

--- src/scene1.js
+++ src/scene1.js
@@ -36,10 +36,12 @@ const scene1 = {
             fish.draw()
             fish.swim()
         })
+        this.hideLogo()
 
         this.sceneLoop()
         this.stingRaySwim()
 
+        document.addEventListener('mousemove', this.hideIntroScreen)
         document.addEventListener('mousemove', this.createDepth)
     },
 
@@ -68,51 +70,52 @@ const scene1 = {
     createShinyCorals() {
 
         // UP
-        this.shinyCorals.push(new ShinyCoral(45, 1, 3, this.p1Speed, 'shiny-1', 'p3', 'pink'))
-        this.shinyCorals.push(new ShinyCoral(42, 1, 5, this.p1Speed, 'shiny-2', 'p2', 'pink'))
-        this.shinyCorals.push(new ShinyCoral(47, -1, 5.5, this.p2Speed, 'shiny-3', 'p2', 'blue'))
-        this.shinyCorals.push(new ShinyCoral(49, 7, 4, this.p2Speed, 'shiny-4', 'p2', 'blue'))
-        this.shinyCorals.push(new ShinyCoral(51, 7, 5, this.p1Speed, 'shiny-5', 'p1', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(50, 2, 3, this.p1Speed, 'shiny-1', 'p3', 'pink'))
+        this.shinyCorals.push(new ShinyCoral(45, 1, 5, this.p1Speed, 'shiny-2', 'p2', 'pink'))
+        this.shinyCorals.push(new ShinyCoral(50, -1, 5.5, this.p2Speed, 'shiny-3', 'p2', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(52, 7, 4, this.p2Speed, 'shiny-4', 'p2', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(54, 7, 5, this.p1Speed, 'shiny-5', 'p1', 'blue'))
 
         // -- NEXT TO WHITE CORAL --
-        this.shinyCorals.push(new ShinyCoral(23, 57, 3.7, this.p1Speed, 'shiny-6', 'p1', 'green'))
-        this.shinyCorals.push(new ShinyCoral(24, 51, 3.4, this.p2Speed, 'shiny-7', 'p2', 'blue'))
-        this.shinyCorals.push(new ShinyCoral(21, 52, 4.2, this.p1Speed, 'shiny-8', 'p2', 'pink'))
-        this.shinyCorals.push(new ShinyCoral(20, 47, 5.4, this.p2Speed, 'shiny-9', 'p3', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(26, 57, 3.7, this.p1Speed, 'shiny-6', 'p1', 'green'))
+        this.shinyCorals.push(new ShinyCoral(27, 51, 3.4, this.p2Speed, 'shiny-7', 'p2', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(24, 52, 4.2, this.p1Speed, 'shiny-8', 'p2', 'pink'))
+        this.shinyCorals.push(new ShinyCoral(23, 47, 5.4, this.p2Speed, 'shiny-9', 'p3', 'blue'))
 
 
         // -- DOWN --
-        this.shinyCorals.push(new ShinyCoral(31, 70, 3.6, this.p2Speed, 'shiny-10', 'p3', 'blue'))
-        this.shinyCorals.push(new ShinyCoral(30, 84, 3.4, this.p1Speed, 'shiny-11', 'p3', 'pink'))
-        this.shinyCorals.push(new ShinyCoral(30, 79, 3, this.p1Speed, 'shiny-12', 'p2', 'green'))
-        this.shinyCorals.push(new ShinyCoral(32, 73, 4, this.p2Speed, 'shiny-13', 'p2', 'blue'))
-        this.shinyCorals.push(new ShinyCoral(31, 72, 2.5, this.p1Speed, 'shiny-14', 'p2', 'pink'))
-        this.shinyCorals.push(new ShinyCoral(34, 79, 4.3, this.p2Speed, 'shiny-15', 'p2', 'blue'))
-        this.shinyCorals.push(new ShinyCoral(30, 77, 5, this.p1Speed, 'shiny-16', 'p1', 'green'))
+        this.shinyCorals.push(new ShinyCoral(33, 70, 3.6, this.p2Speed, 'shiny-10', 'p3', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(32, 84, 3.4, this.p1Speed, 'shiny-11', 'p3', 'pink'))
+        this.shinyCorals.push(new ShinyCoral(32, 79, 3, this.p1Speed, 'shiny-12', 'p2', 'green'))
+        this.shinyCorals.push(new ShinyCoral(34, 73, 4, this.p2Speed, 'shiny-13', 'p2', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(33, 72, 2.5, this.p1Speed, 'shiny-14', 'p2', 'pink'))
+        this.shinyCorals.push(new ShinyCoral(36, 79, 4.3, this.p2Speed, 'shiny-15', 'p2', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(32, 77, 5, this.p1Speed, 'shiny-16', 'p1', 'green'))
 
         // -- OTHERS --
-        this.shinyCorals.push(new ShinyCoral(14, 83, 3, this.p2Speed, 'shiny-17', 'p3', 'blue'))
-        this.shinyCorals.push(new ShinyCoral(-5, 82, 6, this.p2Speed, 'shiny-18', 'p2', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(16, 83, 3, this.p2Speed, 'shiny-17', 'p3', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(-5, 84, 6, this.p2Speed, 'shiny-18', 'p3', 'blue'))
+        this.shinyCorals.push(new ShinyCoral(-5, 74, 4, this.p2Speed, 'shiny-19', 'p3', 'blue'))
 
     },
 
     createSpikedCorals() {
-        this.spikedCorals.push(new SpikedCoral(85, 59, 10, this.p1Speed, 'spiked-1', 'p1'))
+        this.spikedCorals.push(new SpikedCoral(87, 59, 10, this.p1Speed, 'spiked-1', 'p1'))
     },
 
     createSpriteCorals() {
 
         // -- BLUE PINK CORAL --
-        this.spriteCorals.push(new SpriteCoral(-4, 69, 12, this.p1Speed, 'blue-pink-coral-1', 'p1', 13, 'blue-pink-coral'))
-        this.spriteCorals.push(new SpriteCoral(59, 53, 9, this.p2Speed, 'blue-pink-coral-2', 'p2', 13, 'blue-pink-coral'))
+        this.spriteCorals.push(new SpriteCoral(-1, 69, 12, this.p1Speed, 'blue-pink-coral-1', 'p1', 13, 'blue-pink-coral'))
+        this.spriteCorals.push(new SpriteCoral(61, 53, 9, this.p2Speed, 'blue-pink-coral-2', 'p2', 13, 'blue-pink-coral'))
 
         // -- ORANGE HEART CORAL --
-        this.spriteCorals.push(new SpriteCoral(-8, 88, 15, this.p1Speed, 'sprite-coral-1', 'p1', 15, 'orange-heart-coral'))
-        this.spriteCorals.push(new SpriteCoral(71, 70, 13, this.p1Speed, 'sprite-coral-2', 'p1', 15, 'orange-heart-coral'))
+        this.spriteCorals.push(new SpriteCoral(-6, 88, 15, this.p1Speed, 'sprite-coral-1', 'p1', 15, 'orange-heart-coral'))
+        this.spriteCorals.push(new SpriteCoral(74, 70, 13, this.p1Speed, 'sprite-coral-2', 'p1', 15, 'orange-heart-coral'))
 
         // -- WHITE CORAL --
-        this.spriteCorals.push(new SpriteCoral(10, 54, 12, this.p1Speed, 'white-coral-1', 'p1', 15, 'white-coral'))
-        this.spriteCorals.push(new SpriteCoral(75, 80, 15, this.p2Speed, 'white-coral-2', 'p2', 15, 'white-coral'))
+        this.spriteCorals.push(new SpriteCoral(12, 54, 12, this.p1Speed, 'white-coral-1', 'p1', 15, 'white-coral'))
+        this.spriteCorals.push(new SpriteCoral(78, 80, 15, this.p2Speed, 'white-coral-2', 'p2', 15, 'white-coral'))
     },
 
     createStingrays() {
@@ -120,17 +123,17 @@ const scene1 = {
     },
 
     createFish() {
-        this.fish.push(new StaticFish(61, 50, 4, this.p2Speed, 'fish-orange-1', 'p3', 15, 'orange'))
-        this.fish.push(new StaticFish(27, 68, 4.5, this.p2Speed, 'fish-green-1', 'p2', 15, 'green'))
+        this.fish.push(new StaticFish(63, 50, 4, this.p2Speed, 'fish-orange-1', 'p3', 15, 'orange'))
+        this.fish.push(new StaticFish(29, 68, 4.5, this.p2Speed, 'fish-green-1', 'p2', 15, 'green'))
 
         // -- VERTICAL --
         this.fish.push(new PathFish(0, 0, 4.1, this.p2Speed, 'fish-green-2', 'p2', 15, 'green', 'path-3', 0))
-        this.fish.push(new PathFish(0, 0, 3, this.p2Speed, 'fish-red-1', 'p3', 15, 'red', 'path-3', 2))
-        this.fish.push(new PathFish(0, 0, 3.5, this.p2Speed, 'fish-red-2', 'p2', 15, 'red', 'path-4', 2))
+        this.fish.push(new PathFish(0, 0, 3, this.p2Speed, 'fish-red-1', 'p2', 15, 'red', 'path-3', 2))
+        this.fish.push(new PathFish(0, 0, 3.5, this.p2Speed, 'fish-red-2', 'p1', 15, 'pink', 'path-4', 2))
 
         // -- HORIZONTAL --
         this.fish.push(new PathFish(0, 0, 4.5, this.p2Speed, 'fish-orange-3', 'p1', 15, 'red', 'path-5', 0))
-        this.fish.push(new PathFish(0, 0, 2, this.p2Speed, 'fish-red-3', 'p3', 15, 'red', 'path-5', 0.5))
+        this.fish.push(new PathFish(0, 0, 2, this.p2Speed, 'fish-red-3', 'p3', 15, 'pink', 'path-5', 0.5))
 
         this.fish.push(new PathFish(0, 0, 4.1, this.p2Speed, 'fish-green-3', 'p2', 15, 'green', 'path-6', 14))
         this.fish.push(new PathFish(0, 0, 5.5, this.p2Speed, 'fish-purple-q', 'p2', 15, 'purple', 'path-6', 2))
@@ -158,9 +161,36 @@ const scene1 = {
                     alignOrigin: [0.5, 0.5],
                     autoRotate: 90
                 },
-                ease: "linear",
-                delay: 2
+                ease: "linear"
             })
+    },
+
+    hideIntroScreen() {
+        gsap.to('#intro-screen', {
+            duration: 1,
+            opacity: 0,
+            zIndex: -100,
+            delay: 0.5
+        })
+    },
+
+    hideLogo() {
+        gsap.to('#logo', {
+            duration: 1,
+            opacity: 0,
+            delay: 21,
+            onComplete: () => this.showButton()
+        })
+    },
+
+    showButton() {
+        gsap.fromTo('#start-button', { scale: 0.7 }, {
+            visibility: 'visible',
+            scale: 1,
+            opacity: 0.8,
+            duration: 0.5,
+            delay: 1
+        })
     },
 }
 
