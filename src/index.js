935f47eb56a807e3ae7e17eda2e1f2f10050f492
Guillermo Ávila <guille9797@hotmail.com>
2022-11-28 20:02:38 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import scene from './scene'
 
 window.onload = () => {
-    // particlesJS.load('particles-js', 'assets/particles.json', function () {
-    //     console.log('callback - particles.js config loaded');
-    // });
+    particlesJS.load('particles-js', 'assets/particles.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
 
     const startButton = document.querySelector('#start-button')
     startButton.addEventListener('click', () => {
