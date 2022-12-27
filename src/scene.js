b9fb8fee9ed19f2db1e66366807ca029d43298b6
Guillermo Ávila <guille9797@hotmail.com>
2022-12-27 19:25:04 +0100

    added end stingray

--- src/scene.js
+++ src/scene.js
@@ -557,7 +557,6 @@ const scene = {
             end: '55% 100%',
             scrub: 3,
             markers: true,
-            onEnter: () => this.stingraySwim('stingray-path-11', 12, 180, 2)
         })
 
         scene3TL1.to('.water-S3', { top: '-100vh' }, 0)
@@ -585,6 +584,7 @@ const scene = {
             end: '70% 100%',
             scrub: 3,
             markers: true,
+            onEnter: () => this.stingraySwim('stingray-path-11', 12, 180, 0)
         })
 
         scene3TL2.to('.water-S3', { top: '-250vh' }, 0)
@@ -641,13 +641,18 @@ const scene = {
             start: '90% bottom',
             end: '100% 100%',
             scrub: 3,
-            markers: true
+            markers: true,
+            onEnter: () => {
+                this.stingray = new PathStingray(0, 0, 50, 25, 0.5, 'stingray-4', 'scene-3', 'p2', 'fish', 'stingray', 50, { name: 'frontStingrayAnimate', duration: 3 }, 'P2-S3')
+                this.stingraySwim('stingray-path-9', 15, 0, 3)
+            }
         })
 
         scene3TL4.to('.water-S3', { top: '-460vh' }, 0)
         scene3TL4.to('#particles-js', { top: '-455vh', opacity: 1 }, 0)
         scene3TL4.to('#particles-p2-js', { top: '-245vh', opacity: 0.85 }, 0)
         scene3TL4.fromTo('.final', { scale: 1.3 }, { top: '0vh', scale: 1 }, 0)
+        scene3TL4.to('#P1-S3', { top: '-500vh' }, 0)
 
     },
 
